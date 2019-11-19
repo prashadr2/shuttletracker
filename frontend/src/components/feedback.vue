01385be71f20b568be958613698bd85d81ab282c
ruchika-singh <ruchika30.singh@gmail.com>
2019-11-19 16:17:14 -0500

    Added textbox and submit button

--- frontend/src/components/feedback.vue
+++ frontend/src/components/feedback.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="parent">
-      <h1 class="title">Feedback</h1>
-      <p class="subtitle">
-        If you have any comments for the Shuttle Tracker team please enter them below.  
-      </p>
+    <h1 class="title">Feedback</h1>
+    <p class="subtitle">
+      If you have any comments for the Shuttle Tracker team please enter them below.  
+    </p>
+    <div class="field">
+      <textarea class="textarea" placeholder="e.g. I love shuttletracker!">
+      </textarea>
+    </div>
+    <div class="field">
+      <button class="button is-danger is-rounded">Submit</button>
+    </div>  
   </div>
 </template>
+<script lang="ts">
+// This component is the about window that is displayed when the `about` button on the menu is selected
+
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
 
 <style lang="scss" scoped>
 .parent {
